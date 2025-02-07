41ace61d446f24c5bc51f5f96b3b0bcbbc1a5ef6
naturalhacks <naturalhacks@gmail.com>
2025-02-08 03:40:35 +1300

    2025-02-08 03:40:35 naturalhacks@nh-desktop PRESYNC

--- dotfiles/shell/aliases/.pubscript_autogen.sh
+++ dotfiles/shell/aliases/.pubscript_autogen.sh
@@ -2,4 +2,4 @@
 
 alias genalias="/home/naturalhacks/def/cfgrepo/scripts/pub/generate-alias-from-scripts__genalias__.sh"
 alias recgo="/home/naturalhacks/def/cfgrepo/scripts/pub/recompile__recgo__.sh"
-alias updatecfgrepo="/home/naturalhacks/def/cfgrepo/scripts/pub/update-config-repo__updatecfgrepo__.sh"
+alias synccfgrepo="/home/naturalhacks/def/cfgrepo/scripts/pub/update-config-repo__synccfgrepo__.sh"
